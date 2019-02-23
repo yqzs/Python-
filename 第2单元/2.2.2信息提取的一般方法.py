46d52e985e55d09e774965063c694e9a5fc61a71
yqzs <931244228@qq.com>
2019-02-23 19:13:25 +0800

    添加 2.2.2输出语句

--- 第2单元/2.2.2信息提取的一般方法.py
+++ 第2单元/2.2.2信息提取的一般方法.py
@@ -4,11 +4,12 @@ import  re
 r = requests.get('https://python123.io/ws/demo.html')
 soup = BeautifulSoup(r.text,'html.parser')
 '''
-for link in soup.find_all('a'):
+for link in soup.find_all('a'):  #找出a标签包含的链接
 	print(link.get('href'))
-for tag in soup.find_all(True):
+for tag in soup.find_all(True):    
 	print(tag.name)
 for tag in soup.find_all(re.compile('b')):
 	print(tag.name)
-	'''
 print(soup.find_all(id = re.compile('link')))
+'''
+print(soup.find_all(string = re.compile('Python')))  #查找字符串
